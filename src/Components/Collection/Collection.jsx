26314215202e97cb015e66f66f2a5e195b8ececa
Diego <diegogilhz@gmail.com>
2021-10-10 23:18:19 -0500

    responsiveness hotfix

--- src/Components/Collection/Collection.jsx
+++ src/Components/Collection/Collection.jsx
@@ -92,7 +92,9 @@ export default function Collection() {
                                 />
                             ))
                         ) : (
-                            <p> No cards liked yet!</p>
+                            <div className={styles.noCard}>
+                                <p> No cards liked yet!</p>
+                            </div>
                         )
                     )
                 }
