0b350f4854d4212aa93938e0d49c796a8bd7304d
Diego <diegogilhz@gmail.com>
2021-10-10 19:54:36 -0500

    revamps Like logic, adds more styles

--- src/Components/Collection/Card.jsx
+++ src/Components/Collection/Card.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { handleLiked, handleModal } from '../../Redux/actions'
 import styles from './Card.module.css'
 
 export default function Card({ img, title }) {
+    const [active, setActive] = useState(false)
 
     const dispatch = useDispatch()
-    const data = useSelector(state => state?.loadedCards)
+    const data = useSelector(state => state?.likedCards)
 
-    useEffect(()=>{
-        if (data){
-            let req = data.filter(city => city.title)
-            if (req.length && Object.values(req[0])[3]){
-                let target = document.getElementById(title)
-                console.log(target)
-            }
-            else return
-        }
+    const toggleClass = () => {
+        setActive(!active);
+    };
+
+    useEffect(() => {
+    if (data) {
+        let obj = data.filter(city => city.title === title)
+        if (obj.length) setActive(true)
+    }
     },[data])
-
 
     return (
         <div className={styles.wrapper}>
@@ -37,12 +37,14 @@ export default function Card({ img, title }) {
                 <span>{title || 'Title'}</span>
                 {
                     title ? (
-                        <i 
-                            id={title}
-                            className={`fas fa-heart ${styles.heart}`}
-                            onClick={() => dispatch(handleLiked(title))}
-                        />
-                                                
+                            <i 
+                                id={title || null}                            
+                                className={`fas fa-heart ${styles.heart} ${active ? styles.active : styles.inactive}`}
+                                onClick={() => {
+                                    toggleClass()                                    
+                                    dispatch(handleLiked(title))
+                                }}
+                            />
                     ) : (
                         <i className={`fas fa-heart ${styles.dormantHeart}`} />                        
                     )
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { handleLiked, handleModal } from '../../Redux/actions'
 import styles from './Modal.module.css'
 
 export default function Modal() {
+    const [active, setActive] = useState(false)
     const modal = useSelector(state => state?.modal)
+
     const dispatch = useDispatch()
+    const data = useSelector(state => state?.likedCards)
+
+    const toggleClass = () => {
+        setActive(!active);
+    };
+
+    useEffect(() => {
+    if (data) {
+        let obj = data.filter(city => city.title === modal?.title)
+        if (obj.length) setActive(true)
+    }
+    },[data])
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.top}>
-                <i className={`fas fa-times ${styles.closeIcon}`} onClick={() => dispatch(handleModal(null))}></i>
-                <div className={styles.image}>
-                    <img src={modal?.source} alt={modal?.title} />
+                <i 
+                    className={`fas fa-times ${styles.closeIcon}`} 
+                    onClick={() => dispatch(handleModal(null))}
+                />
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src={modal?.source} alt={modal?.title} />
                 </div>
             </div>
             <div className={styles.middle}>
                 <span>{modal?.title}</span>
-                <i className={`fas fa-heart ${styles.heartIcon}`} onClick={() => dispatch(handleLiked(modal?.title))}/>               
+                <i 
+                    className={`fas fa-heart ${styles.heart} ${active ? styles.active : styles.inactive}`} 
+                    onClick={() => {
+                        dispatch(handleLiked(modal?.title))
+                        toggleClass()
+                    }}
+                />               
             </div>
             <div className={styles.bottom}>
                 <p>{modal?.description}</p> 
